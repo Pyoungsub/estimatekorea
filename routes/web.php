3e3cd452829a244f7fddc85df36ffdbb7968bc50
Pyoungsub <ps.shim87@gmail.com>
2025-11-05 11:01:32 +0900

    sitemap

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
 use App\Models\Team;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,3 +25,12 @@ Route::get('/auth/{provider}', function ($provider) {
     return Socialite::driver($provider)->redirect();
 })->name('socialite.redirect');
 Route::get('/auth/{provider}/callback', [App\Http\Controllers\SocialiteLogin::class, 'handleProviderCallback']);
+Route::get('/generate-sitemap', function () {
+    Sitemap::create()
+        ->add(Url::create('/'))
+        ->add(Url::create('/about'))
+        ->add(Url::create('/contact'))
+        ->writeToFile(public_path('sitemap.xml'));
+
+    return 'sitemap.xml generated!';
+});
